8e00c35d66ec4d748eec49d323fba8bace6a6d13
Mehreen-Siddique <mehreensiddique50@gmail.com>
2025-11-16 16:36:12 +0500

    Create the header on the homeScreen.

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -9,14 +9,68 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
+
+
+
+  //header of the screen...
+  Widget buildHeader(){
+    return Row(
+      children: [
+        //Avatar
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(colors: [
+              AppColors.primaryPurple,
+              AppColors.electricBlue
+            ]),
+            border: Border.all(
+              color: AppColors.goldYellow,
+              width: 3
+            )
+          ),
+      child:  Icon(
+        Icons.person,
+        color: Colors.white,
+        size: 32,
+      ),
+    ),
+        SizedBox(width: 12,),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Assalam Alaikum! 👋',
+              style: AppTextStyles.body,
+            ),
+
+
+
+          ],
+        )
+
+      ],
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.darkNavy,
-      body: SafeArea(child:Column(
-        children: [
-
-        ],
+      body: SafeArea(
+          child:
+      SingleChildScrollView(
+        padding: EdgeInsets.all(AppSizes.padding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            buildHeader(),
+            SizedBox(height: 20)
+          ],
+        ),
       )
 
       ),
