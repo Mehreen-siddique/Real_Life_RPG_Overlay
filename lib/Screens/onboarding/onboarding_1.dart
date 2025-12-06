e9a9767a283c3680cfe4ce6d21e123c3d67549ce
Mehreen-Siddique <mehreensiddique50@gmail.com>
2025-12-06 19:36:18 +0500

    Add the onboarding screen.

---
+++ lib/Screens/onboarding/onboarding_1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:real_life_rpg/utils/constants.dart';
+
+class onboarding1 extends StatefulWidget {
+  const onboarding1({super.key});
+
+  @override
+  State<onboarding1> createState() => _onboarding1State();
+}
+
+class _onboarding1State extends State<onboarding1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.lightBackground,
+    );
+  }
+}
