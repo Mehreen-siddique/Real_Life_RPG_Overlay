284af12cf481cf9f4c548a454d0d93d0d996fe20
Mehreen-Siddique <mehreensiddique50@gmail.com>
2025-12-06 21:53:55 +0500

    Add the quest list screen.

---
+++ lib/Screens/QuestList.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Questlist extends StatefulWidget {
+  const Questlist({super.key});
+
+  @override
+  State<Questlist> createState() => _QuestlistState();
+}
+
+class _QuestlistState extends State<Questlist> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
