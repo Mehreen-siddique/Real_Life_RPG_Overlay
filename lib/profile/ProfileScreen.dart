0035ee95a091a0d355b5f26268eeb9f85c28481a
Mehreen-Siddique <mehreensiddique50@gmail.com>
2025-12-08 16:24:09 +0500

    Design the stat card of profile screen.

--- lib/profile/ProfileScreen.dart
+++ lib/profile/ProfileScreen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:real_life_rpg/Models/users.dart';
+import 'package:real_life_rpg/utils/constants.dart';
 
 class profileScreen extends StatefulWidget {
   const profileScreen({super.key});
@@ -16,6 +17,55 @@ class _profileScreenState extends State<profileScreen> {
   void initState() {
     super.initState();
     user = UserModel.dummy();
+  }
+
+  Widget _buildStatCard({
+    required IconData icon,
+    required String label,
+    required int value,
+    required int maxValue,
+    required Color color,
+    bool showProgress = true,
+  }) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: AppColors.whiteBackground,
+        borderRadius: BorderRadius.circular(AppSizes.radius),
+        boxShadow: AppShadows.cardShadow,
+      ),
+      child: Column(
+        children: [
+          Icon(icon, color: color, size: 32),
+          const SizedBox(height: 8),
+          Text(
+            label,
+            style: AppTextStyles.caption,
+          ),
+          const SizedBox(height: 4),
+          Text(
+            '$value',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: color,
+            ),
+          ),
+          if (showProgress) ...[
+            const SizedBox(height: 8),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: LinearProgressIndicator(
+                value: value / maxValue,
+                minHeight: 6,
+                backgroundColor: AppColors.lightBackground,
+                valueColor: AlwaysStoppedAnimation<Color>(color),
+              ),
+            ),
+          ],
+        ],
+      ),
+    );
   }
 
   @override
